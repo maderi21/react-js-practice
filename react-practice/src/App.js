c022f37997370ef1fa9efa932d327a2e70c18f13
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-04 22:06:24 +0200

    editable button

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Accordion from "./components/accordion/accordion";
+import Editable from "./components/editable/Editable";
 
 function App() {
   return (
     <div className="App">
       <Accordion />
+      <Editable />
     </div>
   );
 }
--- react-practice/src/components/editable/Editable.js
+++ react-practice/src/components/editable/Editable.js
@@ -1,25 +1,29 @@
+import { useState } from "react";
+
 const Editable = () => {
   const [editable, setEditable] = useState(false);
-	const [inputValue,setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState("");
 
-const togleEditable = () => setEditable(prevEditable =>!prevEditable)
+  const toggleEditable = () => setEditable((prevEditable) => !prevEditable);
 
-const handleInputValue = (e) => {
-	setInputValue(e.target.value)
-}
+  const handleInputValue = (e) => {
+    setInputValue(e.target.value);
+  };
 
-
-return (
-	<div>
-		{editable ? (
-			<lable>
-				Title
-				<imput type='text' value={inputValue} onChange={handleInputValue} />
-			</lable>
-		) :(
-
-		)}
-	</div>
-)
-
+  return (
+    <div>
+      {editable ? (
+        <lable>
+          Title
+          <imput type="text" value={inputValue} onChange={handleInputValue} />
+        </lable>
+      ) : (
+        <> Title {inputValue}</>
+      )}
+      <br />
+      <button onClick={toggleEditable}>Edit</button>
+    </div>
+  );
 };
+
+export default Editable;
